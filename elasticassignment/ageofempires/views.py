543de51740f3313cfc086806dd0abb9aa0197c0e
coeus <sallahuddinkh@gmail.com>
2021-12-15 15:06:37 +0500

    Adding elastic search

--- elasticassignment/ageofempires/views.py
+++ elasticassignment/ageofempires/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
+from requests.exceptions import ConnectionError,HTTPError,Timeout,TooManyRedirects
+import json
+from elasticsearch import Elasticsearch
 
 # Create your views here.
+
+def civilizations(request):
+    response_data = requests.get("https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations")
+    return_list = response_data.json()
+    civilization_list = return_list['civilizations']
+    es = Elasticsearch()
+    for oo in civilization_list:
+        res = es.index(index="civilizations", body=oo)
+    return render(request, 'ageofempires/civilizations.html' )
